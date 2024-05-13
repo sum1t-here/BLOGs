863953a3697e6ab234b707c9cf0cc9e8fd97e8e2
sumit <mazumdarsumit3@gmail.com>
2024-05-13 16:03:04 +0530

    created signin route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,33 @@
 import { Hono } from 'hono';
 import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
+import { sign } from 'hono/jwt';
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
 
-const app = new Hono();
+app.post('/api/v1/signup', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
 
-app.post('/api/v1/signup', (c) => {
-  return c.text('signup route');
+  const body = await c.req.json();
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.text(jwt);
+  } catch (e) {
+    c.status(403);
+    return c.json({ error: 'error while signing up' });
+  }
 });
 
 app.post('api/v1/signin', (c) => {
