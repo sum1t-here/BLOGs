203fb2c598c52b0a498e65f1ac201073655481a8
sumit <mazumdarsumit3@gmail.com>
2024-05-13 16:27:28 +0530

    limiting middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -34,6 +34,26 @@ app.post('api/v1/signin', (c) => {
   return c.text('signin route');
 });
 
+// this middleware run first then it reaches the other routes of the blog
+app.use('/api/v1/blog/*', async (c, next) => {
+  // get the header
+  // verify the header
+  // if the header is correct, we can proceed
+  // if not we return a 403 status code
+
+  const header = c.req.header('authorization') || '';
+  // Bearer token => ["Bearer", "token"]
+  const token = header.split(' ')[1];
+  // @ts-ignore
+  const response = await verify(token, c.env.JWT_SECRET);
+  if (response.id) {
+    next();
+  } else {
+    c.status(403);
+    return c.json({ error: 'unauthorized' });
+  }
+});
+
 app.put('/api/v1/blog', (c) => {
   return c.text('blog routes');
 });
